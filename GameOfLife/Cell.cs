077cd75e5d92114abae4ecb03767fa493188cb23
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-04-21 21:05:08 +0200

    Delete unused enum

--- GameOfLife/Cell.cs
+++ GameOfLife/Cell.cs
@@ -4,15 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public enum State
-{
-    Live = 1,
-    Dead = 2
-}
-
 namespace GameOfLife
 {
-    class Cell
+    public class Cell
     {
         public int Column { get; set; }
         public int Row { get; set; }
