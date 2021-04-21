34d876897dc122547ab07c0b13332f5d794f9052
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-04-21 20:22:34 +0200

    Create cell class

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum State
+{
+    Live = 1,
+    Dead = 2
+}
+
+namespace GameOfLife
+{
+    class Cell
+    {
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public bool State { get; set; }
+    }
+}
